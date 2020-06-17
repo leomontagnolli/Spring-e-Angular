1533b48c7fb9c3501397955a8ae9a81ec7ecbe23
Leonardo <leomontagnolli@gmail.com>
2020-06-17 17:39:00 -0300

    habilitando cache

--- src/main/java/com/leonardo/financascontrol/FinancascontrolApiApplication.java
+++ src/main/java/com/leonardo/financascontrol/FinancascontrolApiApplication.java
@@ -2,8 +2,10 @@ package com.leonardo.financascontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FinancascontrolApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/leonardo/financascontrol/config/ResourceServerConfig.java
+++ src/main/java/com/leonardo/financascontrol/config/ResourceServerConfig.java
@@ -23,6 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			
 	}
 	
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
