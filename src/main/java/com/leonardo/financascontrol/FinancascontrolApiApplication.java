d44994fa611aec431c15c449369d0260ea930f24
Leonardo <leomontagnolli@gmail.com>
2020-06-21 18:09:55 -0300

    Colocando anotation

--- src/main/java/com/leonardo/financascontrol/FinancascontrolApiApplication.java
+++ src/main/java/com/leonardo/financascontrol/FinancascontrolApiApplication.java
@@ -3,9 +3,11 @@ package com.leonardo.financascontrol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport
 public class FinancascontrolApiApplication {
 
 	public static void main(String[] args) {
