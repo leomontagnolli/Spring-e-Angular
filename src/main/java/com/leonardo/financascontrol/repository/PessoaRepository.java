e78ec5ca3d8e7f74ce03425148c12a043bf577d0
Leonardo <leomontagnolli@gmail.com>
2020-06-25 20:19:11 -0300

    ADicionando metodo ao repository

--- src/main/java/com/leonardo/financascontrol/repository/PessoaRepository.java
+++ src/main/java/com/leonardo/financascontrol/repository/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.leonardo.financascontrol.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.leonardo.financascontrol.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
+	
+	public List<Pessoa> findByNome(String nome);
 }
