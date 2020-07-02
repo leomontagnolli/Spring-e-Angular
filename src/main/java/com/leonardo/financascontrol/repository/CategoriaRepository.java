1cdd9bd80d3d4d2889a1db8305322ced8488d009
Leonardo <leomontagnolli@gmail.com>
2020-07-02 14:39:50 -0300

    Adicionando metodos ao repository

--- src/main/java/com/leonardo/financascontrol/repository/CategoriaRepository.java
+++ src/main/java/com/leonardo/financascontrol/repository/CategoriaRepository.java
@@ -1,10 +1,13 @@
 package com.leonardo.financascontrol.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.leonardo.financascontrol.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+	
+	public List<Categoria> findByNome(String nome);
 
 }
